b070b1031fe85ce4a7678e695fd86ed63546b675
sironeko0930 <117969131+sironeko0930@users.noreply.github.com>
2022-11-22 10:56:20 +0900

    Update app.js

--- qr/qr/js/app.js
+++ qr/qr/js/app.js
@@ -119,7 +119,7 @@ SQR.modal = (() => {
    
 
         
-        alert( value );
+        alert( result );
         })
         window.location.href='https://sironeko0930.github.io/gg/qr/qr/js/users.html?class=' + url ;  
     }
