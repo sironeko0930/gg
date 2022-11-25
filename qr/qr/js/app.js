74bf2de924c82e157855dd1d05dc9c6f07c4339c
shunhayashi <117887043+shunhayashi@users.noreply.github.com>
2022-11-25 11:02:56 +0900

    if

--- qr/qr/js/app.js
+++ qr/qr/js/app.js
@@ -113,9 +113,9 @@ SQR.modal = (() => {
         //「-」で区切って分割する
         var result = str.split('&');//クラス・科目・日時
         
-        document.getElementById('shopinf').innerHTML = "<div>"+result[0]+"</div>";
-        document.getElementById('shopinfo').innerHTML = "<div>"+result[1]+"</div>";
-        document.getElementById('shopin').innerHTML = "<div>"+result[2]+"</div>";
+        // document.getElementById('shopinf').innerHTML = "<div>"+result[0]+"</div>";
+        // document.getElementById('shopinfo').innerHTML = "<div>"+result[1]+"</div>";
+        // document.getElementById('shopin').innerHTML = "<div>"+result[2]+"</div>";
         
 
         if(result[2] == null){
