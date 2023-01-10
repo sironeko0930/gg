c4fe335bdf2be92101ae6d327d6c712e859adfaf
shunhayashi <117887043+shunhayashi@users.noreply.github.com>
2023-01-10 14:53:16 +0900

    sss

--- qr/qr/js/app.js
+++ qr/qr/js/app.js
@@ -112,10 +112,11 @@ SQR.modal = (() => {
         var str = url;
         //「-」で区切って分割する
         var result = str.split('&');//クラス&科目&日時&何限目か
-        alert(result);
+//         alert(result);
         
         alert('https://sironeko0930.github.io/gg/qr/qr/js/users.html?class=' + result[0]
         + "&course=" +result[1]+"&time="+result[2]"&count="+result[3]);
+        
         window.location.href='https://sironeko0930.github.io/gg/qr/qr/js/users.html?class=' + result[0]
         + "&course=" +result[1]+"&time="+result[2]"&count="+result[3];  
     }
